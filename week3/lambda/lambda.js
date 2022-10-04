88b1176a197c134b17989c343074532d4554786b
Roger Sennhauser <roger.sennhauser@students.fhnw.ch>
2022-10-04 16:02:03 +0200

    update week3

--- week3/lambda/lambda.js
+++ week3/lambda/lambda.js
@@ -1,3 +1,6 @@
+const id = x => x;
+const konst = x => y => x;
+const snd = x => y => y;
 
 const id    = x => x;
 const konst = x => y => x;
@@ -15,20 +18,18 @@ const Tuple = n => [
 
 const iOfN = n => i => value => // from n curried params, take argument at position i,
     n === 0
-    ? value
-    : x => iOfN (n-1) (i-1) ( i === 0 ? x : value );
+        ? value
+        : x => iOfN (n-1) (i-1) ( i === 0 ? x : value );
 
+const T = konst; // const T = trueBranch => falseBranch => trueBranch;
+const F = snd; // const F = trueBranch => falseBranch => falseBranch;
 
 const parmStore = n => args => onDone =>  // n args to come
     n === 0
-    ? onDone(args)
-    : arg => parmStore(n - 1)([...args, arg]) (onDone); // store parms in array
+        ? onDone(args)
+        : arg => parmStore(n - 1)([...args, arg]) (onDone); // store parms in array
 
 const Choice = n => [
     ...Array.from( {length:n}, (it, idx) => parmStore(n+1) ([]) (parms => parms[idx+1] (parms[0]) ) ), // ctors
     id
 ];
-
-
-
-
